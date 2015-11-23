89ff7999fc8fb8f89285d37bdfa2d9d5cab97862
josmase <josnase@gmail.com>
2015-11-23 13:02:57 +0100

    linked bootstrap and css files

--- website/index.php
+++ website/index.php
@@ -4,5 +4,35 @@
  * Index file
  */
 include 'stdlib.php';
+?>
 
-// TODO: Implement index file code
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Serble</title>
+
+    <link rel="stylesheet" type="text/css" href="./layout/style.css">
+    <link href="bootstrap-3.3.5-dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <p>asd</p>
+            </div>
+            <div class="col-md-3">
+                <p>asd</p>
+            </div>
+            <div class="col-md-3">
+                <p>asd</p>
+            </div>
+            <div class="col-md-3">
+                <p>asd</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
