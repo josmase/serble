fd2aea55cfd3e6d9c041b29e7a45fee066d4a00c
josmase <josmase@users.noreply.github.com>
2015-11-26 09:18:04 +0100

    angular --> website

---
+++ website/app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name serbleApp
+ * @description
+ * # serbleApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('serbleApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ website/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name serbleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the serbleApp
+ */
+angular.module('serbleApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
