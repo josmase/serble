ffd97279882eefd4a2b8fb5471cc5cdf0b9b2d26
Mustafe1337 <mustafe01@hotmail.com>
2015-12-02 09:49:56 +0100

    new view with controller and css file

---
+++ website/app/scripts/controllers/SignUp.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name serbleApp.controller:SignUp  Ctrl
+ * @description
+ * # SignUpCtrl
+ * Controller of the serbleApp
+ */
+angular.module('serbleApp')
+  .controller('SignUpCtrl', function($scope) {
+
+  });
