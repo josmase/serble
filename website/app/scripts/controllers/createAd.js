616fb9cee5b5336e0f050f0a6ca68f2b431bc8c4
josmase <josmase@users.noreply.github.com>
2015-12-02 08:44:58 +0100

    changed to google icons

--- website/app/scripts/controllers/createAd.js
+++ website/app/scripts/controllers/createAd.js
@@ -9,13 +9,16 @@
  */
 angular.module('serbleApp')
   .controller('CreateAdCtrl', function ($scope) {
-    // function to submit the form after all validation has occurred
-    $scope.submitForm = function (isValid) {
 
-      // check to make sure the form is completely valid
-      if (isValid) {
-        alert('our form is amazing');
-      }
-
+     $scope.category = {
+        value: 'Category'
+      };
+    $scope.type = {
+      value: 'Typ av annons'
     };
+    $scope.main = {
+      submit: function(){
+        console.log($scope.main.header);
+      }
+    }
   });
