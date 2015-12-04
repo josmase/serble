2be9e3acb0a74ddb99154da0420bc1dafb14cfef
josmase <josmase@users.noreply.github.com>
2015-12-04 12:51:32 +0100

    adInfo completed

--- website/app/scripts/controllers/adInfo.js
+++ website/app/scripts/controllers/adInfo.js
@@ -9,5 +9,13 @@
  */
 angular.module('serbleApp')
   .controller('AdInfoCtrl', function ($scope) {
-    $scope.hej="apa"
+
+    $scope.name = 'Kung karl';
+    $scope.description = 'some Lorem ipsum dolor sit amet, usu ad iudico ullamcorper, vel dicat posidonium te, has at habeo aliquip legendos. Eu pri libris iriure aperiam. Eos cu quas dolores. Ad vim purto ancillae necessitatibus, ad ferri justo nonumy mei. Id his labitur patrioque. Eu per vidit aliquip mentitum, mucius utroque cu ius, mel an dico detracto appetere.';
+    $scope.adress = 'Gatan 54';
+    $scope.zipCode = '90333';
+    $scope.city = 'Umeå';
+    $scope.phonenumber = '070-13234543';
+    $scope.email = 'kakmonster@kladdkaka.com'
+
   });
