d23e4405abb8116ad75c96d6ae9826995a7bd6af
josmase <josmase@users.noreply.github.com>
2015-12-07 15:25:56 +0100

    ngInfiniteScroll added as dependencie

--- website/test/karma.conf.js
+++ website/test/karma.conf.js
@@ -35,6 +35,7 @@ module.exports = function(config) {
       'bower_components/lodash/lodash.js',
       'bower_components/angular-google-maps/dist/angular-google-maps.js',
       'bower_components/angular-messages/angular-messages.js',
+      'bower_components/ngInfiniteScroll/build/ng-infinite-scroll.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
