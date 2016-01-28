5579f5c688b06ea77205283d008ccd7b06cdd3ba
Jonas <josnase@gmail.com>
2016-01-28 11:37:24 +0100

    create article and get article now working

--- client/app/scripts/controllers/create-article.js
+++ client/app/scripts/controllers/create-article.js
@@ -8,14 +8,20 @@
  * Controller of the serbleApp
  */
 angular.module('serbleApp')
-  .controller('CreateArticleCtrl', function ($scope,$http) {
+  .controller('CreateArticleCtrl', function ($scope, $http) {
     $scope.articleData = null;
-    $scope.submitForm = function(){
+    $scope.submitForm = function () {
       $http({
         method: 'POST',
         url: 'http://172.16.0.237:3000/articles/create',
         dataType: 'json',
-        data:{'user_id':0,'title':'asd','description': 'asdasd','payout':123,'category':'asdasdasdasdasd'}
+        data: {
+          'user_id': 0,
+          'title': $scope.articleData.title,
+          'description': $scope.articleData.description,
+          'payout': $scope.articleData.price,
+          'category': $scope.articleData.category
+        }
       }).then(function successCallback(response) {
         console.log(response);
         $scope.error = response
