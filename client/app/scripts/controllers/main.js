de8b7c8bf6313131694c35daad387bea8bcbe5a6
Jonas <josnase@gmail.com>
2016-01-29 08:49:48 +0100

    search button added and filter by sell or buy added

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -9,24 +9,22 @@
  */
 angular.module('serbleApp')
   .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
-  $scope.getArticles = function(){
-
-    $http({
-      method: 'GET',
-      url: 'http://172.16.0.237:3000/articles/get',
-      dataType: 'json',
-      params: {'text':$scope.search.text,'order':$scope.search.order,'category':$scope.search.category}
-    }).then(function successCallback(response) {
-     console.log(response);
-      $scope.articles = response
-    }, function errorCallback(response) {
-      console.log(response);
-      $scope.articles = response;
-    });
-  };
-
-
+    $scope.getArticles = function () {
+      $http({
+        method: 'GET',
+        url: 'http://172.16.0.237:3000/articles/get',
+        dataType: 'json',
+        params: {'text': $scope.search.text, 'order': $scope.search.order, 'category': $scope.search.category}
+      }).then(function successCallback(response) {
+        console.log(response);
+        $scope.articles = response
+      }, function errorCallback(response) {
+        console.log(response);
+        $scope.articles = response;
+      });
+    };
+    $scope.search = {};
     $scope.quantity = 20;
-
+    $scope.getArticles();
   }]);
 
