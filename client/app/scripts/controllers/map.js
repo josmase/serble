e7a3e9f4eac4704be70dd3a11c7ff7f1a54b5a7d
Jonas <josnase@gmail.com>
2016-02-01 13:59:35 +0100

    center map on users position and draw 4 markers working

--- client/app/scripts/controllers/map.js
+++ client/app/scripts/controllers/map.js
@@ -14,103 +14,99 @@ angular.module('serbleApp').config(function (uiGmapGoogleMapApiProvider) {
       libraries: 'weather,geometry,visualization'
     });
   })
-  .controller('MapCtrl', function ($scope, uiGmapGoogleMapApi) {
-
-    var createRandomMarker = function (i, bounds, idKey) {
-      var lat_min = bounds.southwest.latitude,
-        lat_range = bounds.northeast.latitude - lat_min,
-        lng_min = bounds.southwest.longitude,
-        lng_range = bounds.northeast.longitude - lng_min;
-
-      if (idKey == null) {
-        idKey = "id";
+  .controller('MapCtrl', function ($scope, uiGmapGoogleMapApi, myMapServices) {
+    $scope.markers = [
+      {
+        id: 1,
+        latitude: 63.8233639,
+        longitude: 20.2642868,
+        title: 'a'
+      },
+      {
+        id: 2,
+        latitude: 64.8233639,
+        longitude: 20.2642868,
+        title: 'b'
+      },
+      {
+        id: 3,
+        latitude: 65.8233639,
+        longitude: 20.2642868,
+        title: 'c'
+      },
+      {
+        id: 4,
+        latitude: 66.8233639,
+        longitude: 20.2642868
       }
+    ];
 
-      var latitude = lat_min + (Math.random() * lat_range);
-      var longitude = lng_min + (Math.random() * lng_range);
-      var ret = {
-        latitude: latitude,
-        longitude: longitude,
-        title: 'm' + i
-      };
-      ret[idKey] = i;
-      return ret;
-    };
-    $scope.randomMarkers = [];
-    // Get the bounds from the map once it's loaded
-    $scope.$watch(function () {
-      return $scope.map.bounds;
-    }, function (nv, ov) {
-      // Only need to regenerate once
-      if (!ov.southwest && nv.southwest) {
-        var markers = [];
-        for (var i = 0; i < 50; i++) {
-          markers.push(createRandomMarker(i, $scope.map.bounds))
-        }
-        $scope.randomMarkers = markers;
+    myMapServices.getCurrentLocation().then(function (data) {
+        $scope.map.center = data;
+
       }
-    }, true);
+    );
     uiGmapGoogleMapApi.then(function (maps) {
+
       $scope.map = {
         center: {
-          latitude: 40.1451,
-          longitude: -99.6680
+          latitude: 43.8233639,
+          longitude: 20.2642868
         },
-        zoom: 4,
+        zoom: 10,
         bounds: {},
-        options: {
-          scrollwheel: false
-        }
+        options: {}
       };
+      $scope.map.options = myMapServices.getMapOptions();
 
+
+      //$scope.map.center = $scope.myCurrentLocation;
       if (typeof _.contains === 'undefined') {
         _.contains = _.includes;
       } //else it exists and we good for lodash3 without doing anything else
     });
+  })
+  .service('myMapServices', function ($http, $q) {
+
+    this.getMapOptions = function () {
+      return {
+        mapOptions: {
+          minZoom: 3,
+          zoomControl: false,
+          draggable: true,
+          navigationControl: false,
+          mapTypeControl: false,
+          scaleControl: false,
+          streetViewControl: false,
+          mapTypeId: google.maps.MapTypeId.ROADMAP,
+          disableDoubleClickZoom: false,
+          keyboardShortcuts: true,
+          styles: [{
+            featureType: "poi",
+            elementType: "labels",
+            stylers: [{
+              visibility: "off"
+            }]
+          }, {
+            featureType: "transit",
+            elementType: "all",
+            stylers: [{
+              visibility: "off"
+            }]
+          }],
+        }
+      };
+    };
+    this.getCurrentLocation = function () {
+      var deferred = $q.defer();
+      navigator.geolocation.getCurrentPosition(function (position) {
+        console.log('position.coords :', position.coords);
+        var myCurrentLocation = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        };
+        deferred.resolve(myCurrentLocation);
+      });
+      return deferred.promise;
+    };
   });
-/* .service('myMapServices', function ($http, $q, uiGmapGoogleMapApi) {
- /!*
- * Function to return map options - used in $scope.map in controller
- *!/
- this.getMapOptions = function () {
- return {
- mapOptions: {
- minZoom: 3,
- zoomControl: false,
- draggable: true,
- navigationControl: false,
- mapTypeControl: false,
- scaleControl: false,
- streetViewControl: false,
- mapTypeId: google.maps.MapTypeId.ROADMAP,
- disableDoubleClickZoom: false,
- keyboardShortcuts: true,
- styles: [{
- featureType: "poi",
- elementType: "labels",
- stylers: [{
- visibility: "off"
- }]
- }, {
- featureType: "transit",
- elementType: "all",
- stylers: [{
- visibility: "off"
- }]
- }],
- }
- };
- };
- this.getCurrentLocation = function () {
- var deferred = $q.defer();
- navigator.geolocation.getCurrentPosition(function (position) {
- console.log('position.coords :', position.coords);
- var myCurrentLocation = {
- latitude: position.coords.latitude,
- longitude: position.coords.longitude
- };
- deferred.resolve(myCurrentLocation);
- });
- return deferred.promise;
- };
- });*/
