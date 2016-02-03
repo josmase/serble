16ae4bb4f36d0e20dee328572dea4c2a35ac0ccf
Jonas <josnase@gmail.com>
2016-02-03 13:35:05 +0100

    test for geocoding

--- client/test/spec/controllers/create-article.js
+++ client/test/spec/controllers/create-article.js
@@ -6,16 +6,28 @@ describe('Controller: CreateArticleCtrl', function () {
   beforeEach(module('serbleApp'));
 
   var CreateArticleCtrl,
-    scope;
+    scope,
+    geocodeService;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope,_geocodeService_) {
     scope = $rootScope.$new();
     CreateArticleCtrl = $controller('CreateArticleCtrl', {
       $scope: scope
-      // place here mocked dependencies
     });
+
+    geocodeService = _geocodeService_;
   }));
+
+  it("should return latitude and longitude for zip code", function () {
+    var articleData = {city: 'umeå', zipCode: 90364};
+    geocodeService.geocode(articleData).then(function (data) {
+      scope.articleData.latitude = data.data.results[0].geometry.location.lat;
+      scope.articleData.longitude = data.data.results[0].geometry.location.lng;
+      expect(scope.articleData.latitude).toBeCloseTo(63.8391708);
+    });
+
+  });
 
 
 });
