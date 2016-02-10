648acc68abeaed833dfcf31cdad5f423ccb645db
Jonas <josnase@gmail.com>
2016-02-10 11:11:03 +0100

    refatored

--- client/app/scripts/controllers/create-article.js
+++ client/app/scripts/controllers/create-article.js
@@ -9,23 +9,40 @@
  */
 angular.module('serbleApp')
   .controller('CreateArticleCtrl', function ($scope, geocodeService, getAndPostArticlesService) {
-    $scope.submitForm = function () {
+    $scope.submitForm = submitForm;
+    $scope.modalShownSuccess = true;
+    $scope.toggleModalSuccess = toggleModalSuccess;
+
+
+    function toggleModalSuccess() {
+      $scope.modalShownSuccess = !$scope.modalShownSuccess;
+    }
+
+    function submitForm() {
       $scope.loading = true;
-      geocodeService.geocode($scope.articleData).then(function (response) {
-        $scope.articleData.latitude = response.data.results[0].geometry.location.lat;
-        $scope.articleData.longitude = response.data.results[0].geometry.location.lng;
-        $scope.articleData.neighborhood = response.data.results[0].address_components[1].long_name;
-        postArticle()
+      geocodeService.geocode($scope.articleData).then(function(response){
+        addLocationAndPost(response);
       });
-    };
+    }
+
+    function addLocationAndPost(response) {
+      addLocationToArticle(response);
+      postArticle();
+    }
+    function addLocationToArticle(response){
+      $scope.articleData.latitude = response.data.results[0].geometry.location.lat;
+      $scope.articleData.longitude = response.data.results[0].geometry.location.lng;
+      $scope.articleData.neighborhood = response.data.results[0].address_components[1].long_name;
+    }
 
     function postArticle() {
       getAndPostArticlesService.postArticleData($scope.articleData)
-        .then(function successCallback(response) {
+        .then(function successCallback() {
           $scope.loading = false;
-        }, function errorCallback(response) {
+        }, function errorCallback() {
           $scope.loading = false;
         });
+
     }
   });
 
