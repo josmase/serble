a38bdf2197b8335d882497a8b7f4079db36227e9
Jonas <josnase@gmail.com>
2016-02-22 16:07:56 +0100

    working test file upload

--- client/app/scripts/controllers/create-article.js
+++ client/app/scripts/controllers/create-article.js
@@ -9,7 +9,7 @@
  */
 angular.module('serbleApp')
   .controller('CreateArticleCtrl', function ($scope, geocodeService, getAndPostArticlesService, Upload) {
-   $scope.articleData = {};
+    $scope.articleData = {};
 
     function toggleModalSuccess() {
       $scope.modalShownSuccess = !$scope.modalShownSuccess;
@@ -59,7 +59,7 @@ angular.module('serbleApp')
     }
 
     function deleteImage() {
-      $scope.articleData.file = null;
+      $scope.file = null;
     }
 
 
@@ -70,48 +70,27 @@ angular.module('serbleApp')
     $scope.modalShownError = false;
     $scope.toggleModalError = toggleModalError;
 
-    var upload = Upload.upload({
-      url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
-      data: {file: $scope.file, username: 'asd'}
-    });
-
-    $scope.sendFile = function () {
-      upload.then(function (resp) {
-        // file is uploaded successfully
-        console.log(resp);
-      }, function (resp) {
-        console.log(resp);
-      }, function (evt) {
-        var progressPercentage = parseInt(100.0 *
-          evt.loaded / evt.total);
-        $scope.log = 'progress: ' + progressPercentage +
-          '% ' + evt.config.data.file.name + '\n' +
-          $scope.log;
-      });
+    // upload later on form submit or something similar
+    $scope.submit = function() {
+      if ($scope.file) {
+        $scope.upload($scope.file);
+      }
     };
 
-    function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
-      console.log(lat1);
-      var R = 6371; // Radius of the earth in km
-      var dLat = deg2rad(lat2-lat1);  // deg2rad below
-      var dLon = deg2rad(lon2-lon1);
-      var a =
-          Math.sin(dLat/2) * Math.sin(dLat/2) +
-          Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-          Math.sin(dLon/2) * Math.sin(dLon/2)
-        ;
-      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-      var d = R * c*1000; // Distance in m
-      return d;
-    }
-
-    function deg2rad(deg) {
-      return deg * (Math.PI/180)
-    }
-
-    var sak = getDistanceFromLatLonInKm(63.8404974,20.1497412,63.8032587,20.2008248);
-    console.log(sak);
-
+    // upload on file select or drop
+    $scope.upload = function (file) {
+      Upload.upload({
+        url: 'http://172.16.0.237:3000/imgupload',
+        data: {file: file, 'username': 'asd'}
+      }).then(function (resp) {
+        console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+      }, function (resp) {
+        console.log('Error status: ' + resp.status);
+      }, function (evt) {
+        var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+        console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+      });
+    };
   });
 
 
