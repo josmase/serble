10e443c8543e2dd0eaecaae8ad66a5eddaa0cc3a
Jonas <josnase@gmail.com>
2016-02-23 14:02:36 +0100

    get author information by author id

--- client/app/scripts/controllers/article.js
+++ client/app/scripts/controllers/article.js
@@ -8,15 +8,21 @@
  * Controller of the serbleApp
  */
 angular.module('serbleApp')
-  .controller('ArticleCtrl', ['$scope', '$routeParams', 'getAndPostArticlesService', function ($scope, $routeParams, getAndPostArticlesService) {
+  .controller('ArticleCtrl', ['$scope', '$routeParams', 'getAndPostArticlesService', function ($location,$scope, $routeParams, getAndPostArticlesService,UserService) {
 
 
     var currentArticleId = parseInt($routeParams.articleId);
-    console.log(currentArticleId);
+
     function getArticle() {
       getAndPostArticlesService.getById(currentArticleId).then(function successCallback(response) {
         console.log(response);
-        $scope.articleInfo = response.data.result[0];
+        if(response.data.result.length > 0){
+          $scope.articleInfo = response.data.result[0];
+          getAuthorById(response.data.result[0].author_id)
+        }
+        else{
+          console.log('No article');
+        }
       }, function errorCallback(response) {
         console.log(response);
       });
@@ -30,7 +36,16 @@ angular.module('serbleApp')
         console.log(response);
       });
     }
-
+    function getAuthorById(authorId){
+      UserService.getById(authorId).then(function(response){
+        if(response.success){
+          $scope.authorInfo = response.data.result[0];
+        }
+        else{
+          console.log('no author');
+        }
+      })
+    }
     $scope.getArticle = getArticle;
     $scope.removeArticle = removeArticle;
 
