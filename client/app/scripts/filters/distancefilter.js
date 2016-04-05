bced436d9bad812f19361442404bd373a9398246
jonas <t4.jonas.lundberg@umea.nti.se>
2016-04-05 14:57:39 +0200

    will return null if input is undefined
    If the user did not accept positional tracking it wold return NaNm and display that. Resulting in a confused user

--- client/app/scripts/filters/distancefilter.js
+++ client/app/scripts/filters/distancefilter.js
@@ -11,7 +11,10 @@
 angular.module('serbleApp')
   .filter('distance', function () {
     return function (input) {
-      if (input >= 1000) {
+      if(typeof input == 'undefined'){
+        return null;
+      }
+      else if (input >= 1000) {
         return (input / 1000).toFixed(2) + 'km';
       } else {
         return (input/1).toFixed(0) + 'm';
