87e051fece34f135f37421d2a6a0560327b4e851
jonas <t4.jonas.lundberg@umea.nti.se>
2016-04-05 12:54:32 +0200

    changed to working state

--- client/app/scripts/services/userservice.js
+++ client/app/scripts/services/userservice.js
@@ -1,57 +1,70 @@
-'use strict';
+(function () {
+  'use strict';
 
+  angular
+    .module('serbleApp')
+    .factory('UserService', UserService);
 
-function UserService($http) {
-  var server = 'http://localhost:3000';
+  UserService.$inject = ['$http'];
+  function UserService($http) {
+    var service = {};
+    var server = 'http://localhost:3000';
+    service.GetAll = GetAll;
+    service.GetById = GetById;
+    service.GetByUsername = GetByUsername;
+    service.Create = Create;
+    service.Update = Update;
+    service.Delete = Delete;
 
-  function handleSuccess(res) {
-    return res.data;
+    return service;
+
+    function GetAll() {
+      return $http.get('/api/users').then(handleSuccess, handleError('Error getting all users'));
+    }
+
+    function GetById(id) {
+      return $http.get('/api/users/' + id).then(handleSuccess, handleError('Error getting user by id'));
+    }
+
+    function GetByUsername(username) {
+      return $http({
+        method: 'GET',
+        url: server + '/user/profile/get',
+        params: {
+          'username': username
+        }
+      }).then(handleSuccess, handleError('Error getting user by username'));
+    }
+
+    function Create(user) {
+      return $http.post(server+'/user/register', user).then(handleSuccess, handleError('Error creating user'));
+    }
+
+    function Update(user) {
+      return $http({
+        method: 'POST',
+        url: server + '/user/profile/update',
+        data: {
+          'data': user
+        }
+      }).then(handleSuccess, handleError('Error updating user'));
+    }
+
+    function Delete(id) {
+      return $http.delete('/api/users/' + id).then(handleSuccess, handleError('Error deleting user'));
+    }
+
+    // private functions
+
+    function handleSuccess(res) {
+      return res.data;
+    }
+
+    function handleError(error) {
+      return function () {
+        return {success: false, message: error};
+      };
+    }
   }
 
-  function handleError(error) {
-    return function () {
-      return {success: false, message: error};
-    };
-  }
-
-  function GetAll() {
-    return $http.get('/api/users').then(handleSuccess, handleError('Error getting all users'));
-  }
-
-  function GetById(id) {
-    return $http.get('/api/users/' + id).then(handleSuccess, handleError('Error getting user by id'));
-  }
-
-  function GetByUsername(username) {
-    return $http({
-      method: 'GET',
-      url: server + '/user/profile/get',
-      params: {
-        'username': username
-      }
-    }).then(handleSuccess, handleError('Error getting user by username'));
-  }
-
-  function Create(user) {
-    return $http.post(server + '/user/register', user).then(handleSuccess, handleError('Error creating user'));
-  }
-
-  function Update(user) {
-    return $http({
-      method: 'POST',
-      url: server + '/user/profile/update',
-      data: {
-        'data': user
-      }
-    }).then(handleSuccess, handleError('Error updating user'));
-  }
-
-  function Delete(id) {
-    return $http.delete('/api/users/' + id).then(handleSuccess, handleError('Error deleting user'));
-  }
-}
-angular
-  .module('serbleApp')
-  .factory('UserService', UserService);
-
-UserService.$inject = ['$http'];
+})();
