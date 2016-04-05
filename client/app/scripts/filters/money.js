a11853a3990535ad1ba8d5e4a696cc249914657a
jonas <t4.jonas.lundberg@umea.nti.se>
2016-04-05 08:13:01 +0200

    filter for currency with currency after the value

---
+++ client/app/scripts/filters/money.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc filter
+ * @name serbleApp.filter:money
+ * @function
+ * @description
+ * # money
+ * Filter in the serbleApp.
+ */
+angular.module('serbleApp')
+  .filter('money', function () {
+    return function (input,currency) {
+      return input+currency;
+    };
+  });
