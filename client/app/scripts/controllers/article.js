3d31f8ca4ddc883be7ad6cdf1fb2394aa4572dc9
jonas <t4.jonas.lundberg@umea.nti.se>
2016-04-05 09:00:09 +0200

    display accurate distance instead of placeholder distance

--- client/app/scripts/controllers/article.js
+++ client/app/scripts/controllers/article.js
@@ -8,46 +8,41 @@
  * Controller of the serbleApp
  */
 angular.module('serbleApp')
-  .controller('ArticleCtrl', ['$scope', 'getAndPostArticlesService','UserService','currentArticleId',
-    function ($scope, getAndPostArticlesService,UserService,currentArticleId) {
+  .controller('ArticleCtrl', ['$scope', 'getAndPostArticlesService', 'UserService', 'currentArticleId', 'myMapServices', 'geocodeService',
+    function ($scope, getAndPostArticlesService, UserService, currentArticleId, myMapServices, geocodeService) {
 
-    function getArticle() {
-      getAndPostArticlesService.getById(currentArticleId).then(function successCallback(response) {
-        console.log(response);
-        if(response.data.result.length > 0){
-          $scope.articleInfo = response.data.result[0];
-          getAuthorById(response.data.result[0].author_id)
-        }
-        else{
-          console.log('No article');
-        }
-      }, function errorCallback(response) {
-        console.log(response);
-      });
-    }
+      function getArticle() {
+        getAndPostArticlesService.getById(currentArticleId).then(function successCallback(response) {
+          if (response.data.result.length > 0) {
+            $scope.articleInfo = response.data.result[0];
+            getAuthorById($scope.articleInfo.author_id);
+            calculateDistance();
+          }
+          else {
+            console.log('No article');
+          }
+        }, function errorCallback(response) {
+          console.log(response);
+        });
+      }
 
-    function removeArticle() {
-      getAndPostArticlesService.removeById(currentArticleId).then(function successCallback(response) {
-        console.log(response);
-        $scope.articleInfo = response;
-      }, function errorCallback(response) {
-        console.log(response);
-      });
-    }
-    function getAuthorById(authorId){
-      UserService.GetById(authorId).then(function(response){
-        if(response.success){
-          $scope.authorInfo = response.data.result[0];
-        }
-        else{
-          console.log('no author');
-        }
-      })
-    }
-    $scope.getArticle = getArticle;
-    $scope.removeArticle = removeArticle;
+      function getAuthorById(authorId) {
+        UserService.GetById(authorId).then(function (response) {
+          if (response.success) {
+            $scope.authorInfo = response.data.result[0];
+          }
+          else {
+            console.log(response);
+          }
+        })
+      }
 
+      function calculateDistance() {
+        myMapServices.getCurrentLocation().then(function (currentLocation) {
+            $scope.articleInfo.distance = geocodeService.getDistanceFromLatLon(currentLocation, $scope.articleInfo);
+          }
+        );
+      }
 
-    getArticle();
-    //removeArticle();
-  }]);
+      getArticle();
+    }]);
