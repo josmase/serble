338b915247dff55db4bee01e579f258825e9bba3
jonas <t4.jonas.lundberg@umea.nti.se>
2016-04-11 16:19:02 +0200

    created a card directive for easier reuse of cards

---
+++ client/app/scripts/directives/carddirective.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name serbleApp.directive:cardDirective
+ * @description
+ * # cardDirective
+ */
+angular.module('serbleApp')
+  .directive('cardDirective', function () {
+      return {
+          restrict: 'E',
+          replace: true, // Replace with the template below
+          templateUrl: "../../templates/card.html"
+      };
+  });
