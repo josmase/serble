e77e04b04d31200c6f9e3db452d144057740d0cb
jonas <josnase@gmail.com>
2016-04-11 17:38:53 +0200

    removed flex since it was not needed

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -68,7 +68,7 @@ angular
   ).run(function ($rootScope, $cookies, $http) {
       // keep user logged in after page refresh
       $rootScope.globals = JSON.parse($cookies.get('globals') || "{}");
-      $rootScope.apiURL = "http://localhost:3000";
+      $rootScope.apiURL = "http://serble.josmase.xyz:3000";
       if ($rootScope.globals.currentUser) {
         $http.defaults.headers.common['Authorization'] = $rootScope.globals.currentUser.token; // jshint ignore:line
       }
