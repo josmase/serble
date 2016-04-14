70b3462a589a57f13184ce68582bbaebac4830f3
jonas <t4.jonas.lundberg@umea.nti.se>
2016-04-14 08:01:50 +0200

    fix error modal being shown after successful retry

--- client/app/scripts/controllers/create-article.js
+++ client/app/scripts/controllers/create-article.js
@@ -8,111 +8,83 @@
  * Controller of the serbleApp
  */
 angular.module('serbleApp')
-  .controller('CreateArticleCtrl', function ($scope, geocodeService, getAndPostArticlesService, Upload, $rootScope) {
-    $scope.articleData = {};
+    .controller('CreateArticleCtrl', function ($scope, geocodeService, getAndPostArticlesService, Upload, $rootScope) {
+        $scope.articleData = {};
 
-    if (!$rootScope.globals.currentUser) {
-      $rootScope.modalShownRegister = true;
-    }
-
-    function toggleModalSuccess() {
-      $scope.modalShownSuccess = !$scope.modalShownSuccess;
-    }
-
-    function toggleModalError() {
-      $scope.modalShownError = !$scope.modalShownError;
-    }
-
-    function postArticle() {
-      getAndPostArticlesService.postArticleData($scope.articleData,$scope.file)
-        .then(function successCallback(response) {
-          $scope.loading = false;
-          if (response.success) {
-            toggleModalSuccess();
-          }
-          else {
-            $scope.errorMessages = response.err;
-            toggleModalError();
-          }
-        }, function errorCallback() {
-          $scope.loading = false;
-          $scope.error = 'Kunde inte nå serble. Vänligen försök igen';
-          toggleModalError();
-        });
-    }
-
-    function addLocationToArticle(response) {
-      if (response.data.results[0]) {
-        $scope.articleData.latitude = response.data.results[0].geometry.location.lat;
-        $scope.articleData.longitude = response.data.results[0].geometry.location.lng;
-        $scope.articleData.neighborhood = response.data.results[0].address_components[2].long_name;
-        return true;
-      } else {
-        return false;
-      }
-
-    }
-
-    function submitForm() {
-      $scope.loading = true;
-
-      geocodeService.geocode($scope.articleData).then(function (response) {
-        if(addLocationToArticle(response) && $scope.file){
-          postArticle();
+        if (!$rootScope.globals.currentUser) {
+            $rootScope.modalShownRegister = true;
         }
-        else{
-         $scope.loading = false;
-          toggleModalError();
-          $scope.errorMessages = ["Adressen gick inte att använda"];
+
+        function toggleModalSuccess() {
+            $scope.modalShownSuccess = !$scope.modalShownSuccess;
+            $scope.modalShownError = false;
         }
-      });
 
-    }
-
-    function deleteImage() {
-      $scope.file = null;
-    }
-
-    $scope.submit = function () {
-      if ($scope.file) {
-        $scope.upload($scope.file);
-      }
-      else {
-        $scope.loading = false;
-      }
-    };
-
-    // upload on file select or drop
-    $scope.upload = function (file) {
-      Upload.upload({
-        url: $rootScope.apiURL + '/upload',
-        data: {file: file, data: $scope.articleData}
-      }).then(function (response) {
-        $scope.loading = false;
-        if (response.data.success) {
-          toggleModalSuccess();
-          $scope.modalShownError = false;
+        function toggleModalError() {
+            $scope.modalShownError = !$scope.modalShownError;
+            $scope.modalShownSuccess = false;
         }
-        else {
-          $scope.errorMessages = response.data.err;
-          toggleModalError();
+
+        function postArticle() {
+            getAndPostArticlesService.postArticleData($scope.articleData, $scope.files)
+                .then(function successCallback(response) {
+                    $scope.loading = false;
+                    if (response.success) {
+                        toggleModalSuccess();
+                    }
+                    else {
+                        $scope.errorMessages = response.err;
+                        toggleModalError();
+                    }
+                }, function errorCallback() {
+                    $scope.loading = false;
+                    $scope.error = 'Kunde inte nå serble. Vänligen försök igen';
+                    toggleModalError();
+                });
         }
-      }, function (resp) {
-        $scope.loading = false;
-        $scope.error = 'Kunde inte nå serble. Vänligen försök igen';
-        toggleModalError();
-      }, function (evt) {
-        var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-      });
-    };
 
-    $scope.deleteImage = deleteImage;
-    $scope.submitForm = submitForm;
-    $scope.modalShownSuccess = false;
-    $scope.toggleModalSuccess = toggleModalSuccess;
-    $scope.modalShownError = false;
-    $scope.toggleModalError = toggleModalError;
+        function addLocationToArticle(response) {
+            if (response.data.results.length > 0) {
+                $scope.articleData.latitude = response.data.results[0].geometry.location.lat;
+                $scope.articleData.longitude = response.data.results[0].geometry.location.lng;
+                
+                $scope.articleData.neighborhood = response.data.results[0].address_components[0].long_name;
 
-  });
+                console.log($scope.articleData);
+
+                return true;
+            }
+            return false;
+
+        }
+
+        function submitForm() {
+            $scope.loading = true;
+
+            geocodeService.geocode($scope.articleData).then(function (response) {
+                if (addLocationToArticle(response)) {
+                    postArticle();
+                }
+                else {
+                    $scope.loading = false;
+                    toggleModalError();
+                    $scope.errorMessages = ["Adressen gick inte att använda"];
+                }
+            });
+
+        }
+
+        function deleteImage() {
+            $scope.files = null;
+        }
+
+        $scope.deleteImage = deleteImage;
+        $scope.submitForm = submitForm;
+        $scope.modalShownSuccess = false;
+        $scope.toggleModalSuccess = toggleModalSuccess;
+        $scope.modalShownError = false;
+        $scope.toggleModalError = toggleModalError;
+
+    });
 
 
