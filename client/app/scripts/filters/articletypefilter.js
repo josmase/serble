aee98357375be80509db99354a41985a14d10a51
jonas <t4.jonas.lundberg@umea.nti.se>
2016-04-19 09:19:26 +0200

    filter for type of article

---
+++ client/app/scripts/filters/articletypefilter.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc filter
+ * @name serbleApp.filter:articleTypeFilter
+ * @function
+ * @description
+ * # articleTypeFilter
+ * Filter in the serbleApp.
+ */
+angular.module('serbleApp')
+    .filter('articleTypeFilter', function () {
+        return function (input) {
+            if (input == 0) {
+                return 'Behöver hjälp'
+            }
+            else if (input == 1) {
+                return 'Kan hjälpa'
+            }
+        };
+    });
