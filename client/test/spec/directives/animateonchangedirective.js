76237e53311c1badb473b15c2e25b75919683101
jonas <t4.jonas.lundberg@umea.nti.se>
2016-04-22 09:08:49 +0200

    init test file

---
+++ client/test/spec/directives/animateonchangedirective.js
@@ -0,0 +1,14 @@
+'use strict';
+
+describe('Directive: animateOnChangeDirective', function () {
+
+  // load the directive's module
+  beforeEach(module('serbleApp'));
+
+  var
+    scope;
+
+  beforeEach(inject(function ($rootScope) {
+    scope = $rootScope.$new();
+  }));
+});
