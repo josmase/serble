dd976d82fd1df33780f0eafe04948c383cb676c8
c16jlg <josnase@gmail.com>
2018-01-02 04:23:58 +0100

    Allow cors

--- server/app.js
+++ server/app.js
@@ -80,9 +80,19 @@ var certificate = "asd";
 
 var credentials = {key: privateKey, cert: certificate};
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'hejsan.ml');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+};
+
 var app = express()
     .use(cors())
+    .use(allowCrossDomain())
     .use(bodyParser.json());
+
 
 app.listen(PORT_DEFAULT);
 
